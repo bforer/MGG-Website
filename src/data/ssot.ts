2c9774c250964926c06a1fd19566c98f687e2c41
bforer <brendan@brandboom.co.za>
2026-01-26 09:00:28 +0200

    updated mobile header contact number

--- src/data/ssot.ts
+++ src/data/ssot.ts
@@ -108,7 +108,7 @@ export const organization: OrganizationData = {
 				}
 			},
 			contact: {
-				landline: "011 608 4094",
+				landline: "021 815 9880",
 				email: "info@mgg.co.za",
 				staff: []
 			},
